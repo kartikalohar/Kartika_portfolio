b03e0a12e9838811e5778c696e952730b73d21eb
kartikalohar <kitukartika@gmail.com>
2025-12-12 18:48:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle mobile menu
+const toggleBtn = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
